48064dd999ff28fd3a7af36e7b73feedd0401b76
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2021-03-09 00:22:13 -0300

    navbar responsiva com animação de abrir e fechar

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const navbar = document.getElementById('navbar');
+const fundoNavbar = document.getElementById('fundo-navopen');
+
+const navbarButtons = document.querySelectorAll('.navbar-menu a');
+const btnMenu = document.getElementById('btn-abrirmenu');
+const btnFecharMenu = document.getElementById('closemenu-btn');
+
+//Animação da navbar abrindo e fechando, e do fundo escuro sumindo ou aparecendo
+const abrirFecharMenu = () => {
+    console.log('oi')
+    if (navbar.classList.contains('ativo')) {
+        navbar.classList.remove('ativo');
+        fundoNavbar.classList.remove('ativo');
+    }
+    else {
+        navbar.classList.add('ativo');
+        fundoNavbar.classList.remove('oculto');
+        setTimeout(function() {
+            fundoNavbar.classList.add('ativo');
+        }, 50);
+        
+    }
+}
+
+//evento para ativar o display: none; no fundo escuro atras da navbar apenas após a transição terminar
+fundoNavbar.addEventListener('transitionend', function(){
+    if (!fundoNavbar.classList.contains('ativo')) {
+        fundoNavbar.classList.add('oculto');
+    }
+});
+
+navbarButtons.forEach(element => {
+    element.onclick = abrirFecharMenu;
+});
+
+btnMenu.onclick = abrirFecharMenu;
+btnFecharMenu.onclick = abrirFecharMenu;
